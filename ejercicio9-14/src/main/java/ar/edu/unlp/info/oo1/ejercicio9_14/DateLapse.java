ed657b71300108e3b4939fc84f996d766e41ca4a
visinojuancruz <visinodeveloper@gmail.com>
2023-03-07 17:18:33 -0300

    End all methods in DateLapse class

--- ejercicio9-14/src/main/java/ar/edu/unlp/info/oo1/ejercicio9_14/DateLapse.java
+++ ejercicio9-14/src/main/java/ar/edu/unlp/info/oo1/ejercicio9_14/DateLapse.java
@@ -1,5 +1,26 @@
 package ar.edu.unlp.info.oo1.ejercicio9_14;
+import java.time.LocalDate;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class DateLapse {
 
+	private LocalDate from;
+	private LocalDate to;
+	
+	public LocalDate getFrom() {
+		return this.from;
+	}
+	
+	public LocalDate getTo() {
+		return this.to;
+	}
+	
+	public int sizeInDays() {
+		return (int) DAYS.between(this.getFrom(), this.getTo());
+	}
+	
+	public boolean includesDate(LocalDate unaFecha) {
+		return this.sizeInDays() > (int) DAYS.between(this.getFrom(), unaFecha);
+	}
+	
 }
